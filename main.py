fd0227c6adce3df3d512aeacaf6acabd32e52c96
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-03 10:38:37 +0200

    index

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import pylab
 import wave
 import os
+import pandas
+
+emotion_dict = {
+    '01': 'neutral',
+    '02': 'calm',
+    '03': 'happy',
+    '04': 'sad',
+    '05': 'angry',
+    '06': 'fearful',
+    '07': 'disgust',
+    '08': 'surprised'
+}
+
+intensity_dict = {
+    '01': 'normal',
+    '02': 'strong'
+}
+
+statement_dict = {
+    '01': 'Kids are talking by the door',
+    '02': 'Dogs are sitting by the door'
+}
+
+data_index = []
 
 
 def get_wav_info(filename):
@@ -12,6 +36,27 @@ def get_wav_info(filename):
     wav.close()
 
     return sound_info, frame_rate
+
+
+def add_index_entry(file_path):
+    file_name = os.path.basename(file_path).replace('.png', '').split('-')
+
+    gender = 'F'
+    if (int(file_name[6]) % 2 > 0):
+        gender = 'M'
+
+    data_index.append({
+        'file_path': file_path,
+        'emotion': emotion_dict[file_name[2]],
+        'intensity': intensity_dict[file_name[3]],
+        'statement': statement_dict[file_name[4]],
+        'gender': gender
+    })
+
+
+def save_index(file_path):
+    df = pandas.DataFrame(data_index)
+    df.to_csv(file_path)
 
 
 def graph_spectrogram(filename):
@@ -30,11 +75,13 @@ def iterate_dirs(dir_name):
         if (os.path.isdir(file_path)):
             iterate_dirs(file_path)
         elif (file_path.endswith('.wav')):
-            if (os.path.isfile(file_path.replace('.wav', '.png'))):
-                return
+            target_filepath = file_path.replace('.wav', '.png')
+            add_index_entry(target_filepath)
 
-            graph_spectrogram(file_path)
+            if (os.path.isfile(target_filepath) == False):
+                graph_spectrogram(file_path)
 
 
 input_dir = './data/'
 iterate_dirs(input_dir)
+save_index('index.csv')
