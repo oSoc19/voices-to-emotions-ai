2494545bedb4f2f0e2b2d94fd8ecbc02781df0c2
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-03 08:04:52 +0200

    process all input

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pylab
 import wave
 import os
 
+
 def get_wav_info(filename):
+    print 'Reading: ' + filename
     wav = wave.open(filename, 'r')
     frames = wav.readframes(-1)
     sound_info = pylab.fromstring(frames, 'int16')
@@ -11,15 +13,18 @@ def get_wav_info(filename):
 
     return sound_info, frame_rate
 
+
 def graph_spectrogram(filename, inputDir, outputDir):
+    if (filename.endswith('.wav') == False): return
+
     sound_info, frame_rate = get_wav_info(inputDir + filename)
     pylab.figure(num=None, figsize=(19, 12))
     pylab.subplot(111)
     pylab.specgram(sound_info, Fs=frame_rate)
     pylab.savefig(outputDir + filename.replace('.wav', '.png'))
 
+
 files = os.listdir('./input_data/')
 for i in range(0, len(files)):
     file = files[i]
     graph_spectrogram(file, './input_data/', './output_data/')
-
