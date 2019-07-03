c2636a153926dbacb461e63e361227e56dcbe911
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-03 09:57:09 +0200

    read subdirs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_wav_info(filename):
-    print 'Reading: ' + filename
+    print('Reading: ' + filename)
     wav = wave.open(filename, 'r')
     frames = wav.readframes(-1)
     sound_info = pylab.fromstring(frames, 'int16')
@@ -14,17 +14,24 @@ def get_wav_info(filename):
     return sound_info, frame_rate
 
 
-def graph_spectrogram(filename, inputDir, outputDir):
-    if (filename.endswith('.wav') == False): return
-
-    sound_info, frame_rate = get_wav_info(inputDir + filename)
+def graph_spectrogram(filename):
+    sound_info, frame_rate = get_wav_info(filename)
     pylab.figure(num=None, figsize=(19, 12))
     pylab.subplot(111)
     pylab.specgram(sound_info, Fs=frame_rate)
-    pylab.savefig(outputDir + filename.replace('.wav', '.png'))
+    pylab.savefig(filename.replace('.wav', '.png'))
 
 
-files = os.listdir('./input_data/')
-for i in range(0, len(files)):
-    file = files[i]
-    graph_spectrogram(file, './input_data/', './output_data/')
+def iterate_dirs(dir_name):
+    files = os.listdir(dir_name)
+    for i in range(0, len(files)):
+        file_name = files[i]
+        file_path = os.path.join(dir_name, file_name)
+        if (os.path.isdir(file_path)):
+            iterate_dirs(file_path)
+        elif (file_path.endswith('.wav')):
+            graph_spectrogram(file_path)
+
+
+input_dir = './data/'
+iterate_dirs(input_dir)
