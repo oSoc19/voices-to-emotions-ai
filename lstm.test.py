1fe3278caae069ecf6b71cbd98c46414138b162f
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-05 16:55:55 +0200

    add some validation and such, pretty accurate on non trained data with background noiise

--- lstm.test.py
+++ lstm.test.py
@@ -3,17 +3,28 @@ import tflearn
 import lstm_speech_data
 import numpy as np
 import gc
-from random import shuffle
+import os
+
+emotion_dict = {
+    0: 'angry',
+    1: 'calm',
+    2: 'disgust',
+    3: 'fearful',
+    4: 'happy',
+    5: 'sad',
+    6: 'neutral'
+}
 
 learning_rate = 0.0001
-training_iters = 1000
+training_iters = 2
 training_epochs = 10
-batch_size = 64
 
 width = 20  # mfcc features
 height = 500  # (max) length of utterance
 classes = 7
-dataset = lstm_speech_data.load_dataset()
+dataset_folder = os.path.abspath('./data/youtube')
+dataset = lstm_speech_data.load_dataset(dataset_folder=dataset_folder)
+batch_size = 100
 
 # Network building
 net = tflearn.input_data([None, width, height])
@@ -21,42 +32,26 @@ net = tflearn.lstm(net, 128, dropout=0.8)
 net = tflearn.fully_connected(net, classes, activation='softmax')
 net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
 
-# Training
+# Load model
 model = tflearn.DNN(net, tensorboard_verbose=1)
-
-# Load previous model to improve training
 model.load("lstm.model")
 
 gc.collect()
 
-for i in range(0, training_iters):
-    print('Current Iter: #', str(i))
-
-    print('Loading data...')
-    shuffle(dataset)
-    trainX, trainY = lstm_speech_data.mfcc_get_batch(dataset, batch_size=batch_size)
-    shuffle(dataset)
-    testX, testY = lstm_speech_data.mfcc_get_batch(dataset, batch_size=batch_size)
-
-    print('Training model...')
-    model.fit(trainX, trainY, n_epoch=training_epochs, validation_set=(testX, testY), show_metric=True,
-              batch_size=batch_size)
-
-    gc.collect()
-
-# Save model
-print('Saving model...')
-model.save("lstm.model")
-
 print('Evaluating model...')
-
-shuffle(dataset)
-evalX, evalY = lstm_speech_data.mfcc_get_batch(dataset, batch_size=batch_size)
+evalX, evalY = lstm_speech_data.mfcc_get_batch(dataset, batch_size=batch_size, dataset_folder=dataset_folder)
 predictions = model.predict(evalX)
 accuracy = 0
 for prediction, actual in zip(predictions, evalY):
     predicted_class = np.argmax(prediction)
     actual_class = np.argmax(actual)
+
+    print('emotion percentages:')
+    for i in range(0, len(prediction)):
+        p = prediction[i]
+        print(emotion_dict[i] + ': ' + str(round(p * 10000) / 100) + '%')
+    print('Predicted:', emotion_dict[predicted_class], ',', 'Actual:', emotion_dict[actual_class])
+
     if (predicted_class == actual_class):
         accuracy += 1
 
--- lstm_speech_data.py
+++ lstm_speech_data.py
@@ -1,18 +1,18 @@
 import os
 import librosa
-from random import shuffle
 import numpy as np
+import math
 
 data_dir = os.path.abspath('./data')
 
 emotion_dict = {
-    'W': 0,
-    'L': 1,
-    'E': 2,
-    'A': 3,
-    'F': 4,
-    'T': 5,
-    'N': 6
+    'W': 0,  # angry
+    'L': 1,  # calm
+    'E': 2,  # disgust
+    'A': 3,  # fearful
+    'F': 4,  # happy
+    'T': 5,  # sad
+    'N': 6  # neutral
 }
 
 
@@ -21,32 +21,48 @@ def dense_to_one_hot(labels_dense, num_classes=7):
     return np.eye(num_classes)[labels_dense]
 
 
-def mfcc_batch_generator(batch_size=10):
+def load_audio_data(file_path):
+    wave, sr = librosa.load(file_path, mono=True)
+    wave_frag_offsets = librosa.effects.split(wave, top_db=35)
+
+    results = []
+    for offsets in wave_frag_offsets:
+        wave_fragment = wave[offsets[0]:offsets[1]]
+        mfcc = librosa.feature.mfcc(wave_fragment, sr)
+        _, y_size = mfcc.shape
+
+        splitted_mfcc = np.array_split(mfcc, math.ceil(y_size / 500), axis=1)
+
+        for short_mfcc in splitted_mfcc:
+            short_mfcc = np.pad(short_mfcc, ((0, 0), (0, 500 - len(short_mfcc[0]))), mode='constant', constant_values=0)
+            results.append(np.array(short_mfcc))
+
+    return results
+
+
+def load_dataset(dataset_folder=os.path.join(data_dir, 'german')):
+    return os.listdir(dataset_folder)
+
+
+def mfcc_get_batch(files, dataset_folder=os.path.join(data_dir, 'german'), batch_size=10):
     batch_features = []
     labels = []
-    dataset_folder = os.path.join(data_dir, 'german')
-    files = os.listdir(dataset_folder)
 
-    while True:
-        print("loaded batch of %d files" % len(files))
-        shuffle(files)
+    for wav in files:
+        if not wav.endswith(".wav"):
+            continue
 
-        for wav in files:
-            if not wav.endswith(".wav"):
-                continue
+        file_path = os.path.join(dataset_folder, wav)
+        label = dense_to_one_hot(emotion_dict[wav[5:6]], 7)
+        audio_data = load_audio_data(file_path)
 
-            file_path = os.path.join(dataset_folder, wav)
-            wave, sr = librosa.load(file_path, mono=True)
-            wave_frag_offsets = librosa.effects.split(wave, top_db=35)
+        for mfcc in audio_data:
+            labels.append(label)
+            batch_features.append(mfcc)
 
-            for offsets in wave_frag_offsets:
-                wave_fragment = wave[offsets[0]:offsets[1]]
-                label = dense_to_one_hot(emotion_dict[wav[5:6]], 7)
-                labels.append(label)
-                mfcc = librosa.feature.mfcc(wave_fragment, sr)
-                mfcc = np.pad(mfcc, ((0, 0), (0, 500 - len(mfcc[0]))), mode='constant', constant_values=0)
-                batch_features.append(np.array(mfcc))
-                if len(batch_features) >= batch_size:
-                    yield batch_features, labels  # basic_rnn_seq2seq inputs must be a sequence
-                    batch_features = []  # Reset for next batch
-                    labels = []
+            # Return early if the batch_size has been satisfied
+            if len(batch_features) >= batch_size:
+                return batch_features, labels
+
+    print('NOT ENOUGH DATA TO SATISFY BATCH_SIZE')
+    return batch_features, labels
